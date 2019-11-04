fea752b6aca2d31b455fbb02d492922e979d12fc
juliana <phamovajuliana@gmail.com>
2019-11-04 21:58:04 +0100

    vytvorenie serveru

---
+++ server.c
@@ -0,0 +1,51 @@
+//
+// Created by juliana on 11/4/19.
+//
+#include "sys/socket.h"
+#include "netinet/in.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+#include "arpa/inet.h"
+#include "unistd.h"
+
+main () {
+    //vytvorenie socketu
+    int sock_desc = socket(AF_INET,SOCK_STREAM,0);
+    char c='\n';
+    char *p_buf;
+    if(sock_desc == -1)
+    {
+        printf("cannot create socket!\n");
+        return 0;
+    }
+    //nastavenie socketu
+    struct sockaddr_in server;
+    memset(&server,0,sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY; //s ktorej ip sa da prihlasit
+    server.sin_port = htons(59836);
+    if (bind(sock_desc, (struct sockaddr*)&server, sizeof(server)) != 0)
+    {
+        printf("cannot bind socket!\n");//nepresiel port
+        close(sock_desc);
+        return 0;
+    }
+    if (listen(sock_desc,4) != 0)
+    {
+        printf("cannot listen on socket!\n");
+        close(sock_desc);
+        return 0;
+    }
+    struct  sockaddr_in client;
+    memset(&client,0, sizeof(client));
+    socklen_t len = sizeof(client);
+    int temp_sock_desc = accept(sock_desc,(struct sockaddr*)&client, &len);
+    if (temp_sock_desc == -1) //docastny sock_desc
+    {
+        printf("cannot accept client!\n");
+        close(sock_desc);
+        return 0;
+    }
+
+}

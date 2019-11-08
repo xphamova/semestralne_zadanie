1317acbef2a2b9c480413580cdc751e8818e6d17
juliana <phamovajuliana@gmail.com>
2019-11-08 13:56:31 +0100

    vytvorenie serveru

--- client.c
+++ client.c
@@ -1,18 +1,58 @@
-//
-// Created by juliana on 11/2/19.
-//
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-#include "stdio.h"
-#include "unistd.h"
-#include "stdlib.h"
+int main(){
 
-//TODO: dopln
-//TODO: dopln
+    int i;
+    int forkval;
 
-int main() {
-    for (int i = 1; i < 5; i++) {
-//        int a = 0;
-        printf("fee");
+    forkval = fork();
+
+    if(forkval == 0){
+
+        //scitac
+        exit(1);
+
+    } else {
+
+        forkval = fork();
+
+        if (forkval == 0) {
+
+            //vypisovac
+            exit(1);
+
+        } else {
+
+            for (i = 0; i < 4; i++) {
+
+                forkval = fork();
+
+                if (forkval == 0) {
+
+                    break;
+
+                }
+
+            }
+
+            if (forkval == 0) {
+
+                int Dh = i * 25;
+                int Hh =
+
+                        printf("Ja som child %d a mam interval %d az %d\n", i, i * 25, i * 25 + 25);
+
+                exit(1);
+
+            } else {
+
+                printf("\nJa som parent\n");
+
+            }
+
+            return 0;
+        }
     }
-    return 0;
 }

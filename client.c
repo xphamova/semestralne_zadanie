6983342db46f72423426c897f1d9eeab9d7d37be
juliana <phamovajuliana@gmail.com>
2019-11-08 14:36:32 +0100

    vytvorenie serveru

--- client.c
+++ client.c
@@ -9,7 +9,7 @@ int main(){
 
     forkval = fork();
 
-    if(forkval == 0){
+    if(forkval == 0){ // proces na scitavanie
 
         //scitac
         exit(1);
@@ -18,18 +18,18 @@ int main(){
 
         forkval = fork();
 
-        if (forkval == 0) {
+        if (forkval == 0) { //proces na vypisanie
 
             //vypisovac
             exit(1);
 
         } else {
 
-            for (i = 0; i < 4; i++) {
+            for (i = 0; i < 4; i++) { //vytvaranie klientov
 
                 forkval = fork();
 
-                if (forkval == 0) {
+                if (forkval == 0) { //aby sa nemnozili
 
                     break;
 
@@ -37,18 +37,18 @@ int main(){
 
             }
 
-            if (forkval == 0) {
+            if (forkval == 0) {  //klienti
 
                 int Dh = i * 25;
                 int Hh =
 
                         printf("Ja som child %d a mam interval %d az %d\n", i, i * 25, i * 25 + 25);
 
-                exit(1);
+                exit(1); //zabijanie
 
             } else {
 
-                printf("\nJa som parent\n");
+                printf("\nJa som parent\n"); //server
 
             }
 

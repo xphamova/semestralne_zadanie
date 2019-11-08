8832558065128a4c96f6e151f4f7996ad0915883
juliana <phamovajuliana@gmail.com>
2019-11-08 15:28:32 +0100

    vytvorenie procesov

--- client.c
+++ client.c
@@ -2,57 +2,69 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(){
+int main()
+{
 
-    int i;
+    int id_klienta,dolna_hranica,horna_hranica,rozsah;
+    int Dolna_podhranica,Horna_podhranica;
     int forkval;
 
+    printf("Zadaj rozsah na vypocet, velkost rozsah musi byt delitelna 4 a dolna hranica parna!\n");
+    scanf("%d %d",&dolna_hranica,&horna_hranica);
+    rozsah = horna_hranica - dolna_hranica;
+    if( (rozsah % 4 == 0) && (dolna_hranica % 2 == 0))
+    {
     forkval = fork();
 
-    if(forkval == 0){ // proces na scitavanie
-
+    if(forkval == 0)
+      {                            // proces na scitavanie
         //scitac
         exit(1);
 
-    } else {
+      } else
+          {
 
         forkval = fork();
 
-        if (forkval == 0) { //proces na vypisanie
+        if (forkval == 0)
+          {                      //proces na vypisanie
 
             //vypisovac
             exit(1);
 
-        } else {
+          } else
+              {
 
-            for (i = 0; i < 4; i++) { //vytvaranie klientov
+            for (id_klienta = 0; id_klienta < 4; id_klienta++)
+            {                        //vytvaranie klientov
 
                 forkval = fork();
 
-                if (forkval == 0) { //aby sa nemnozili
-
+                if (forkval == 0)
+                {                    //aby sa nemnozili
                     break;
-
                 }
 
             }
 
-            if (forkval == 0) {  //klienti
+            if (forkval == 0)
+            {                        //klienti
 
-                int Dh = i * 25;
-                int Hh =
+                Dolna_podhranica = id_klienta*(rozsah/4) + dolna_hranica+1;
+                Horna_podhranica = id_klienta*(rozsah/4) + (rozsah/4) + dolna_hranica;
 
-                        printf("Ja som child %d a mam interval %d az %d\n", i, i * 25, i * 25 + 25);
+                      //  printf("Ja som child %d a mam interval %d az %d\n", i, i * 25, i * 25 + 25);
 
                 exit(1); //zabijanie
 
-            } else {
-
+            } else
+                {
                 printf("\nJa som parent\n"); //server
-
-            }
+                }
 
             return 0;
-        }
+                }
+          }
     }
+    return 0;
 }

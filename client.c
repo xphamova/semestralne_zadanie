ae3b2ab5b51a3b690ac8b93b634ab6bedbebe989
juliana <phamovajuliana@gmail.com>
2019-11-15 23:08:24 +0100

    spustenie vlakien

--- client.c
+++ client.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
+ void * thread1()
+ {
+
+     int i,prvocislo=2,j,k=0,l;
+     int prvocisla[100];
+
+
+    // int Dolna_podhranica = *((int *)udaj);
+    // int Horna_podhranica = *((int *)udaj);
+     for (i=2;i<=50;)
+     {
+         if(prvocislo>=50)
+             break;
+         for(j= 2; j<= prvocislo;j++)
+         {
+             if(prvocislo%j==0)
+                 break;
+         }
+         if (j == prvocislo)
+         {
+             if (prvocislo>=10)
+             {
+                 prvocisla[k] = prvocislo;
+                 k++;
+             }
+             i++;
+         }
+         prvocislo++;
+     }
+     for(l=0;l<k;l++)
+     {
+         printf("%d  ",prvocisla[l]);
+     }
+     printf("\n");
+
+ }
 
 int main()
 {
@@ -45,6 +82,10 @@ int main()
                     break;
                 }
 
+
+
+
+
             }
 
             if (forkval == 0)
@@ -54,6 +95,12 @@ int main()
                 Horna_podhranica = id_klienta*(rozsah/4) + (rozsah/4) + dolna_hranica;
 
                        printf("Ja som child %d a mam interval %d az %d\n", id_klienta,Dolna_podhranica,Horna_podhranica);
+                int udaje[2];
+                udaje[0]=Dolna_podhranica;
+                udaje[1]=Horna_podhranica;
+               // void *udaj = &udaje[2];
+                pthread_t tid1;
+                pthread_create(&tid1, NULL, thread1 , NULL);
 
                 exit(1); //zabijanie
 
@@ -66,6 +113,7 @@ int main()
                 }
           }
     } else printf("Zadal si zly interval!\n");
+
 
     return 0;
 }

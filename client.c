d7a4aa5d3104970bc9ee2596f8f000af752489a1
juliana <phamovajuliana@gmail.com>
2019-11-18 20:29:50 +0100

    primanie sprav

--- client.c
+++ client.c
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 
 int k=0;
+//1.vlakno...................................................................................................................................................
 void * find(void *udaj) {
 
     int i, prvocislo = 2, j, l;
@@ -47,7 +48,7 @@ void * find(void *udaj) {
     }
     return (void *) output;
 }
-
+//2.vlakno........................................................................................................................................
  void *sum(void *rec)
  {
   int *pole=(int*)rec;
@@ -61,29 +62,17 @@ void * find(void *udaj) {
  }
 
 
-
+//----------------------------------------------------------------------------------------------------------------------
 int main()
  {
 
-    int id_klienta,dolna_hranica,horna_hranica,rozsah;
-    int Dolna_podhranica,Horna_podhranica;
-    int forkval;
+    int id_klienta,dolna_hranica,horna_hranica,rozsah,Dolna_podhranica,Horna_podhranica,forkval;
 
     printf("Zadaj rozsah na vypocet, velkost rozsahu musi byt delitelna 4 a dolna hranica parna!\n");
     scanf("%d %d",&dolna_hranica,&horna_hranica);
     rozsah = horna_hranica - dolna_hranica;
     if( (rozsah % 4 == 0) && (dolna_hranica % 2 == 0)) //ci je splnena podmienka
     {
-    forkval = fork();
-
-    if(forkval == 0)
-      {                            // proces na scitavanie
-        //scitac
-        exit(1);
-
-      } else
-          {
-
         forkval = fork();
 
         if (forkval == 0)
@@ -97,7 +86,6 @@ int main()
 
             for (id_klienta = 0; id_klienta < 4; id_klienta++)
             {                        //vytvaranie klientov
-
                 forkval = fork();
 
                 if (forkval == 0)
@@ -105,33 +93,28 @@ int main()
                     break;
                 }
 
-
-
-
-
             }
 
             if (forkval == 0)
-            {                        //klienti
+            {                        //klienti_________________________________________________________________________
 
                 Dolna_podhranica = id_klienta*(rozsah/4) + dolna_hranica+1;
                 Horna_podhranica = id_klienta*(rozsah/4) + (rozsah/4) + dolna_hranica;
 
                        printf("Ja som child %d a mam interval %d az %d\n", id_klienta,Dolna_podhranica,Horna_podhranica);
                 int udaje[2];
-                udaje[0]=Dolna_podhranica;
-                udaje[1]=Horna_podhranica;
+                    udaje[0]=Dolna_podhranica;
+                    udaje[1]=Horna_podhranica;
                 void *udaj;
                 void *rec;
                 void *rec2;
                 int *message;
                 udaj=(&udaje);
-                pthread_t tid1;
-                pthread_t tid2;
-                pthread_create(&tid1, NULL, find ,(void*)udaj);//spustenie vlakna
-                pthread_join(tid1,&rec);
-                pthread_create(&tid2,NULL,sum,(void*)rec);
-                pthread_join(tid2,&rec2);
+                pthread_t tid1,tid2;
+                    pthread_create(&tid1, NULL, find ,(void*)udaj);//spustenie vlakna
+                        pthread_join(tid1,&rec);
+                    pthread_create(&tid2,NULL,sum,(void*)rec);
+                        pthread_join(tid2,&rec2);
                 message=(int*)rec2;
                 printf("vysledok: %d\n",*message);
 
@@ -163,7 +146,7 @@ int main()
 
             } else
                 {
-                printf("\nJa som parent\n"); //server
+                printf("\nJa som parent\n"); //server____________________________________________________________
                     //vytvorenie socketu
                     int sock_desc = socket(AF_INET,SOCK_STREAM,0);
                     if(sock_desc == -1)
@@ -173,11 +156,11 @@ int main()
                     }
                     //nastavenie socketu
                     struct sockaddr_in server;
-                    memset(&server,0,sizeof(server));
-                    server.sin_family = AF_INET;
+                    memset(&server,0,sizeof(server)); //naplni nulami
+                    server.sin_family = AF_INET; //aky
                     server.sin_addr.s_addr = INADDR_ANY; //s ktorej ip sa da prihlasit
-                    server.sin_port = htons(59836);
-                    if (bind(sock_desc, (struct sockaddr*)&server, sizeof(server)) != 0)
+                    server.sin_port = htons(59836); //nastavenie portu
+                    if (bind(sock_desc, (struct sockaddr*)&server, sizeof(server)) != 0) // ked neprejde port je obsadeny
                     {
                         printf("cannot bind socket!\n");//nepresiel port
                         close(sock_desc);
@@ -189,38 +172,27 @@ int main()
                         close(sock_desc);
                         return 0;
                     }
-                    int client,client1,client2,client3;
-                   client= accept(sock_desc,NULL,NULL);
-                    client1= accept(sock_desc,NULL,NULL);
-                    client2= accept(sock_desc,NULL,NULL);
-                    client3= accept(sock_desc,NULL,NULL);
+                    int client[4],id;
+                    int client_response[4];
+                    int finally_sum = 0;
+                    for (id = 0;id < 4;id++)
+                    {
+                        client[id]= accept(sock_desc,NULL,NULL); //akceptovanie klientov
+                        recv(client[id],&client_response[id], sizeof(client_response[id]),0); //pijatie sprav
+                        finally_sum += client_response[id];
+                        printf("%d  ",client_response[id]);
+                    }
+                    printf("\nkonecny vysledok: %d \n",finally_sum);
 
 
 
-
-
-                    int client_response=0,client_response_1,client_response_2,client_response_3;
-
-                        recv(client1,&client_response, sizeof(client_response),0);
-                        printf("%d  ",client_response);
-                    recv(client,&client_response_1, sizeof(client_response_1),0);
-                    printf("%d  ",client_response_1);
-                    recv(client2,&client_response_2, sizeof(client_response_2),0);
-                    printf("%d  ",client_response_2);
-                    recv(client3,&client_response_3, sizeof(client_response_3),0);
-                    printf("%d  ",client_response_3);
                     close(sock_desc);
                     printf("server disconnected\n");
-
-
-
-
+                    exit(1);
 
                 }
-
-            //return 0;
                 }
-          }
+
     } else printf("Zadal si zly interval!\n");
 
 

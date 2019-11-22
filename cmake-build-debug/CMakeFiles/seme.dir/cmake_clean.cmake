8aef60eea81a1769781f88462271f48b03cd483e
juliana <phamovajuliana@gmail.com>
2019-11-22 17:00:14 +0100

    staci dorobit casovac, pozriet sa na vypocet prvocisel

---
+++ cmake-build-debug/CMakeFiles/seme.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/seme.dir/client.c.o"
+  "seme"
+  "seme.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/seme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

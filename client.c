66b2e1d2f115f3ce8fc7d20db1943a01b9ec2c66
juliana <phamovajuliana@gmail.com>
2019-11-22 16:26:49 +0100

    staci dorobit casovac, pozriet sa na vypocet prvocisel

--- client.c
+++ client.c
@@ -8,91 +8,89 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/shm.h>
-#include <semaphore.h>
-#include <sys/mman.h>
 #include <signal.h>
-sig_atomic_t volatile stop = 1;
-void vypis();
-void proces();
 
+sig_atomic_t volatile stop = 1;
+
+void write_function();
+void process_start();
 
 
 //1.vlakno...................................................................................................................................................
-void * find(void *input) {
+void * find(void *input)
+{
+         int i, j, l,count=0,primes[100];
+         int *output;
+         int *interval = (int *) (input); //novy pointer na int
 
+    //shared memory
+        key_t key = 26;
+        int shmid = shmget(key, sizeof(int),0666|IPC_CREAT);
+        int *shm = (int*)shmat(shmid,NULL,0);
+        int *s;
+            s=shm;
 
-    int i, prvocislo = 2, j, l,k=0;
-    int prvocisla[100];
-    int *output;
-    int *hranice = (int *) (input); //novy pointer na int
+        int First_possible_prime = *(interval); //nahram dolnu hranicu
+        int Last_possible_prime = *(interval + 1); //nahram hornu hranicu
+        int prime = First_possible_prime;
 
-    int Prve_prvocislo = *(hranice); //nahram dolnu hranicu
-    int Posledne_prvocislo = *(hranice + 1); //nahram hornu hranicu
-    for (i =  2; i <= Posledne_prvocislo;) //hladam prvocisla
-    {
-        if (prvocislo >= Posledne_prvocislo+1) //aby to skoncilo tak kde malo
-            break;
-        for (j = 2; j <= prvocislo; j++) {
-            if (prvocislo % j == 0) // zistujem delitelnosti cisla
-                break;
-        }
-        if (j == prvocislo) //
+    //find primes
+        for (i = First_possible_prime; i <= Last_possible_prime; i++)
         {
-            if (prvocislo >= Prve_prvocislo-1) //zapisovanie az od kedy by malo
+
+            for (j = 2; j <= prime; j++)
             {
-                prvocisla[k] = prvocislo; // zapisanie prvocisel do pola
-                k++;
+                if (prime % j == 0) // zistujem delitelnosti cisla
+                    break;
             }
-            i++;
+            if (j == prime) //
+            {
+                primes[count] = prime; // zapisanie prvocisel do pola
+                     count++;
+            }
+        prime++;
         }
-        prvocislo++;
-       // printf("%d",k);
-        key_t key = 27;
-        int shmid_sec = shmget(key, sizeof(int),0666|IPC_CREAT);
-        int *shm_sec = (int*)shmat(shmid_sec,NULL,0);
-        int *s_sec;
-        s_sec=shm_sec;
-        *s_sec = k;
 
+        *s=count; //write into shared memory
 
+    //create dynamic memory
+         output = (int *) malloc(count * sizeof(int));
+            if (output == NULL)
+            {
+                printf("Memory not allocated.\n");
+                exit(0);
+            }
 
-    }
-    output = (int *) malloc(k * sizeof(int));
-    if (output == NULL) {
-        printf("Memory not allocated.\n");
-        exit(0);
-    }
-    for (l = 0; l < k; l++) {
-        output[l] = prvocisla[l];
-
-    }
-
+    //write to dynamic memory
+        for (l = 0; l < count; l++)
+          {
+             output[l] = primes[l];
+             printf("%d ", primes[l]);
+          }
 
     return (void *) output;
 }
+
 //2.vlakno........................................................................................................................................
- void *sum(void *rec)
+ void *sum(void *send_primes)
  {
-     key_t key= 28;
-     int *s_sec;
-     int shmid_sec = shmget(key, sizeof(int),0666);
-     int *shm_sec;
-     shm_sec = (int *) shmat(shmid_sec, NULL, 0);
-     s_sec =  shm_sec;
+     //shared memory
+        key_t key= 26;
+        int shmid = shmget(key, sizeof(int),0666);
+        int *shm;
+        int *s;
+            shm = (int *) shmat(shmid, NULL, 0);
+             s =  shm;
 
-  int *pole=(int*)rec;
-  int *suma;
-   suma = (int*)malloc(sizeof(int));
-   suma[0]=0;
-   int k = s_sec;
-   //printf("%d",k);
+         int *pole_prime=(int*)send_primes;
+         int *sum;
+              sum = (int*)malloc(sizeof(int));
+              sum[0]=0;
 
-   int i;
-   for (i=0;i < k; i++)
-       suma[0] += pole[i];
+         for (int i=0;i < *s; i++)
+             sum[0] += pole_prime[i];
 
-
-     return (void*)suma;
+     return (void*)sum;
  }
 
 
@@ -100,150 +98,160 @@ void * find(void *input) {
 int main()
  {
 
-    int id_klienta,dolna_hranica,horna_hranica,rozsah,Dolna_podhranica,Horna_podhranica,forkval;
-
-
-
+    int id_client,low_range,up_range,range,client_low_range,client_up_range,forkval;
 
     printf("Zadaj rozsah na vypocet, velkost rozsahu musi byt delitelna 4!\n");
-    scanf("%d %d",&dolna_hranica,&horna_hranica);
-    rozsah = horna_hranica - dolna_hranica;
-    if( (rozsah % 4 == 0) ) //ci je splnena podmienka
+    scanf("%d %d", &low_range, &up_range);
+   // printf("prvocisla: ");
+
+    range = up_range - low_range;
+    if( (range % 4 == 0) ) //ci je splnena podmienka
     {
         forkval = fork();
         int pid_v = forkval;
         if (forkval == 0)
           {                     //proces na vypisanie-------------------------------------------------------------------
-          //  sleep(2);
                 while (1)
-              signal(SIGHUP,vypis);
-
-
-           exit(1);
-//.....................................................................................................................
+                    signal(SIGHUP, write_function);
+                               //.......................................................................................
           } else
               {
-
-            int child_pid[4];
-            for (id_klienta = 0; id_klienta < 4; id_klienta++)
-            {                        //vytvaranie klientov
-                forkval = fork();
-                child_pid[id_klienta]=forkval;
-                if (forkval == 0)
-                {                    //aby sa nemnozili
-
-                    break;
+                int child_pid[4];
+        //make client
+                for (id_client = 0; id_client < 4; id_client++)
+                {
+                    forkval = fork();
+                    child_pid[id_client]=forkval;
+                         if (forkval == 0)
+                          {                    //aby sa nemnozili
+                             break;
+                          }
                 }
 
-            }
+                 if (forkval == 0)
+                    {
+    //klienti__________________________________________________________________________________________________________
+                        client_low_range = id_client * (range / 4) + low_range + 1;
+                        client_up_range = id_client * (range / 4) + (range / 4) + low_range;
+                        // printf("Ja som child %d a mam interval %d az %d\n", id_client,client_low_range,client_up_range);
 
-            if (forkval == 0)
-            {                        //klienti_________________________________________________________________________
+                    //wait on server
+                          while(stop)
+                              signal(SIGHUP, process_start);
 
-                Dolna_podhranica = id_klienta*(rozsah/4) + dolna_hranica+1;
-                Horna_podhranica = id_klienta*(rozsah/4) + (rozsah/4) + dolna_hranica;
+                            int interval_range[2];
+                              interval_range[0]=client_low_range;
+                              interval_range[1]=client_up_range;
+                            void *send_range;
+                            void *receive_primes;
+                            void *client_sum;
+                            int *message;
+                                 send_range=(&interval_range);
 
-                  //     printf("Ja som child %d a mam interval %d az %d\n", id_klienta,Dolna_podhranica,Horna_podhranica);
-                while(stop)
-                    signal(SIGHUP,proces);
-                int udaje[2];
-                    udaje[0]=Dolna_podhranica;
-                    udaje[1]=Horna_podhranica;
-                void *udaj;
-                void *rec;
-                void *rec2;
-                int *message;
-                udaj=(&udaje);
-                pthread_t tid1,tid2;
-                    pthread_create(&tid1, NULL, find ,(void*)udaj);//spustenie vlakna
-                        pthread_join(tid1,&rec);
-                    pthread_create(&tid2,NULL,sum,(void*)rec);
-                        pthread_join(tid2,&rec2);
-                message=(int*)rec2;
-                printf("vysledok %d: %d\n",id_klienta,*message);
+                            pthread_t tid1,tid2;
+                                pthread_create(&tid1, NULL, find ,(void*)send_range);//spustenie vlakna
+                                     pthread_join(tid1,&receive_primes);
+                                pthread_create(&tid2,NULL,sum,(void*)receive_primes);
+                                     pthread_join(tid2,&client_sum);
+                             message=(int*)client_sum;
+                           // printf("vysledok %d: %d\n",id_klienta,*message);
 
-
-                //pipojenie na sockety..............................................................................
+                //pipojenie na sockety..................................................................................
 
                     // vytvorenie socketu
-                int sock_desc = socket(AF_INET,SOCK_STREAM,0);
-                if (sock_desc == -1)
-                {
-                    printf("cannot create socket!\n");
-                    return 0;
-                }
+                                 int sock_desc = socket(AF_INET,SOCK_STREAM,0);
+                                  if (sock_desc == -1)
+                                    {
+                                        printf("cannot create socket!\n");
+                                        return 0;
+                                    }
+
                     //nastavenie socketu
-                struct sockaddr_in client;
-                memset(&client,0, sizeof(client));
-                client.sin_family = AF_INET;
-                client.sin_addr.s_addr = inet_addr("127.0.0.1");
-                client.sin_port=htons(59836);
+                                 struct sockaddr_in client;
+                                  memset(&client,0, sizeof(client));
+                                  client.sin_family = AF_INET;
+                                  client.sin_addr.s_addr = inet_addr("127.0.0.1");
+                                  client.sin_port=htons(59836);
+
                     //pripojenie socketu
-                if (connect(sock_desc,(struct sockaddr*)&client, sizeof(client)) != 0)
-                {
-                    printf("cannot connect to server\n");
-                    close(sock_desc);
-                }
+                                  if (connect(sock_desc,(struct sockaddr*)&client, sizeof(client)) != 0)
+                                  {
+                                     printf("cannot connect to server\n");
+                                     close(sock_desc);
+                                  }
 
-                send(sock_desc,message, sizeof(message),0); // posielanie dat
-                exit(1); //zabijanie
+                    //posielanie sprav
+                                 send(sock_desc,message, sizeof(message),0); // posielanie dat
+                                 exit(1); //zabijanie
 
-            } else
-                {
-               // printf("\nJa som parent\n"); //server____________________________________________________________
+                    } else
+                        {
+     //server__________________________________________________________________________________________________________
+                     // printf("\nJa som parent\n");
                     //vytvorenie socketu
-                    kill(child_pid[0],SIGHUP);
-                    int sock_desc = socket(AF_INET,SOCK_STREAM,0);
-                    if(sock_desc == -1)
-                    {
-                        printf("cannot create socket!\n");
-                        return 0;
-                    }
-                    //nastavenie socketu
-                    struct sockaddr_in server;
-                    memset(&server,0,sizeof(server)); //naplni nulami
-                    server.sin_family = AF_INET; //aky
-                    server.sin_addr.s_addr = INADDR_ANY; //s ktorej ip sa da prihlasit
-                    server.sin_port = htons(59836); //nastavenie portu
-                    if (bind(sock_desc, (struct sockaddr*)&server, sizeof(server)) != 0) // ked neprejde port je obsadeny
-                    {
-                        printf("cannot bind socket!\n");//nepresiel port
-                        close(sock_desc);
-                        return 0;
-                    }
-                    if (listen(sock_desc,4) != 0)
-                    {
-                        printf("cannot listen on socket!\n");
-                        close(sock_desc);
-                        return 0;
-                    }
-                    int client[4],id;
-                    int client_response[4];
-                    int *finally_sum;
-                    finally_sum = (int*)malloc(sizeof(int));
-                    finally_sum[0]=0;
-                    for (id = 0;id < 4;id++)
-                    {
-                        client[id]= accept(sock_desc,NULL,NULL); //akceptovanie klientov
-                        recv(client[id],&client_response[id], sizeof(client_response[id]),0); //pijatie sprav
-                        finally_sum[0] += client_response[id];
-                        kill(child_pid[id+1],SIGHUP);
-                   //     printf("%d  ",client_response[id]);
-                    }
-                    printf("\nfinal result: %d \n",finally_sum[0]);
-                    key_t key = 26;
-                    int shmid = shmget(key, sizeof(int),0666|IPC_CREAT);
-                    int *shm = (int*)shmat(shmid,NULL,0);
-                   int *s;
-                   s=shm;
-                   *s = finally_sum[0];
-                   kill(pid_v,SIGHUP);
-                    close(sock_desc);
-                    printf("server disconnected\n");
-                    sleep(2);
-                    exit(1);
+                                 kill(child_pid[0],SIGHUP);
+                                int sock_desc = socket(AF_INET,SOCK_STREAM,0);
+                                if(sock_desc == -1)
+                                {
+                                  printf("cannot create socket!\n");
+                                    return 0;
+                                 }
 
-                }
+                    //nastavenie socketu
+                                 struct sockaddr_in server;
+                                 memset(&server,0,sizeof(server)); //naplni nulami
+                                 server.sin_family = AF_INET; //aky
+                                 server.sin_addr.s_addr = INADDR_ANY; //s ktorej ip sa da prihlasit
+                                 server.sin_port = htons(59836); //nastavenie portu
+                    //bind
+                                 if (bind(sock_desc, (struct sockaddr*)&server, sizeof(server)) != 0) // ked neprejde port je obsadeny
+                                  {
+                                    printf("cannot bind socket!\n");//nepresiel port
+                                    close(sock_desc);
+                                     return 0;
+                                  }
+                    //listen
+                                 if (listen(sock_desc,4) != 0)
+                                  {
+                                     printf("cannot listen on socket!\n");
+                                     close(sock_desc);
+                                         return 0;
+                                  }
+
+
+                                int accept_client[4],id;
+                                int client_response[4];
+                                int *finally_sum;
+                                     finally_sum = (int*)malloc(sizeof(int));
+                                     finally_sum[0]=0;
+
+                    //accept,recv
+                                 for (id = 0;id < 4;id++)
+                                 {
+                                     accept_client[id]= accept(sock_desc, NULL, NULL); //akceptovanie klientov
+                                    recv(accept_client[id], &client_response[id], sizeof(client_response[id]), 0); //pijatie sprav
+                                    finally_sum[0] += client_response[id];
+                                    kill(child_pid[id+1],SIGHUP);
+                                   //     printf("%d  ",client_response[id]);
+                                 }
+
+                                // printf("\nfinal result: %d \n",finally_sum[0]);
+
+                    //shared memory
+                                key_t key = 26;
+                                int shmid = shmget(key, sizeof(int),0666|IPC_CREAT);
+                                int *shm = (int*)shmat(shmid,NULL,0);
+                                int *s;
+                                    s=shm;
+                                    *s = finally_sum[0];
+
+                    //start write process
+                                 kill(pid_v,SIGHUP);
+                                 close(sock_desc);
+                                 // printf("server disconnected\n");
+
+                             exit(1);
+                        }
                 }
 
     } else printf("Zadal si zly interval!\n");
@@ -251,20 +259,23 @@ int main()
 
     return 0;
 }
-void vypis()
+void write_function()
 {
-
-   key_t key= 26;
+  //shared memory
+    key_t key= 26;
     int *s;
     int shmid = shmget(key, sizeof(int),0666);
     int *shm;
     shm = (int *) shmat(shmid, NULL, 0);
     s =  shm;
-    printf("Sucer prvocisel je: %d",*s);
+
+        printf("\nSucet prvocisel je: %d",*s);
+        shmctl(shmid,IPC_RMID,NULL);
+
     exit(1);
 }
 
-void proces()
+void process_start()
 {
     stop = 0;
 }
